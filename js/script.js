ab4ca233b8b696e00143a5471b06d1fd7b17243c
AvetisDN <avetisdn@gmail.com>
2021-05-13 17:53:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector('#menu-btn')
+const menu = document.querySelector('#menu')
+const shadow = document.querySelector('#menu-shadow')
+
+menuBtn.onclick = function() {
+    menu.classList.toggle('show')
+    shadow.classList.toggle('show')
+}
+
+shadow.onclick = function() {
+    menu.classList.remove('show')
+    shadow.classList.remove('show')
+}
+
+$('.carousel').owlCarousel({
+    margin:16,
+    responsiveClass:true,
+    responsive: {
+        0: {
+            items: 2
+        },
+        768: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        },
+        1400: {
+            items: 6
+        }
+    }
+})
